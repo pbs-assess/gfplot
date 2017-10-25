c59378b34dcb8776ca0462ce6e1c5285bbde6ab6
Sean Anderson <sean@seananderson.ca>
2017-10-24 22:31:36 -0700

    Start main figure layout

---
+++ R/layout.R
@@ -0,0 +1,28 @@
+m <- rbind(
+  c(1, 2),
+  c(3, 4),
+  c(3, 4),
+  c(5, 6),
+  c(7, 7)
+)
+
+# layout.show(7)
+
+layout(m)
+par(mar = c(0,0,0,0), oma = c(0.5, 0.5, 0.5, 0.5))
+
+plot(1, type = "n", axes = FALSE)
+box(col = "grey50")
+plot(1, type = "n", axes = FALSE)
+box(col = "grey50")
+plot(1, type = "n", axes = FALSE)
+box(col = "grey50")
+plot(1, type = "n", axes = FALSE)
+box(col = "grey50")
+plot(1, type = "n", axes = FALSE)
+box(col = "grey50")
+plot(1, type = "n", axes = FALSE)
+box(col = "grey50")
+plot(1, type = "n", axes = FALSE)
+box(col = "grey50")
+
