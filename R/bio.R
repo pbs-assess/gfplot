ebe9e6ecd620a9d95886bb7d3fbf9882cb02a36b
Sean Anderson <sean@seananderson.ca>
2017-10-27 15:05:18 -0700

    Speeeed

--- R/bio.R
+++ R/bio.R
@@ -8,49 +8,57 @@ if (!exists("dbio")) {
   dbio <- mutate(dbio, year = lubridate::year(trip_start_date))
 }
 
-torun <- names(rev(sort(table(dbio$species_common_name)))[seq_len(50)])
+bio <- group_by(dbio, species_common_name, species_science_name, year) %>% 
+  summarise(
+    n_age = sum(!is.na(age)),
+    n_length = sum(!is.na(length)),
+    n_weight = sum(!is.na(weight)),
+    n_maturity = sum(!is.na(maturity))
+  ) %>% ungroup
+
+if (!exists("dsurv")) {
+  dsurv <- readRDS("../../Dropbox/dfo/data/all-survey-dat.rds")
+  names(dsurv) <- tolower(names(dsurv))
+  dsurv$species_common_name <- tolower(dsurv$species_common_name)
+  dsurv$species_science_name <- tolower(dsurv$species_science_name)
+  dsurv <- mutate(dsurv, year = lubridate::year(trip_start_date))
+}
+
+surv <- group_by(dsurv, species_common_name, species_science_name, 
+  survey_series_desc, year) %>% 
+  summarise(
+    n_fe_weight = sum(!is.na(catch_weight)),
+    n_fe_count = sum(!is.na(catch_count))
+  ) %>% ungroup
+
+surveys <- data.frame(survey_series_desc = c("Hecate Strait Synoptic Survey", 
+  "IPHC Longline Survey", 
+  "PHMA Rockfish Longline Survey - Outside North", 
+  "PHMA Rockfish Longline Survey - Outside South",
+  "Queen Charlotte Sound Synoptic Survey", 
+  "West Coast Haida Gwaii Synoptic Survey",
+  "West Coast Vancouver Island Synoptic Survey"),
+  surv_short = c("HS", "IPHC", "PHMAN", "PHMAS", "QCS", "WCHG", "WCVI"), 
+  stringsAsFactors = FALSE)
+
+###############
+years <- seq(1996, 2016)
+labs <- intersect(seq(1800, 3000, 2), years)
+# col <- viridis::viridis(200, option = "D")
+col <- colorRampPalette(RColorBrewer::brewer.pal(9, "Blues"))(200)
+# col <- colorRampPalette(RColorBrewer::brewer.pal(9, "Spectral"))(200)
+grid_col <- "grey75"
+box_col <- "grey55"
+grid_lwd <- 1.3
+surv_col <- "grey33"
+axis_col <- "grey30"
+###############
+
+torun <- names(rev(sort(table(dbio$species_common_name)))[seq_len(100)])
 
 for (i in seq_along(torun)) {
   
-  ###########
   common_name <- torun[i]
-  ##########
-  
-  # sp <- filter(dbio, species_common_name %in% "shortraker rockfish")
-  # pairs(sp[,c("sex", "age", "length", "maturity", "weight")])
-  
-  bio <- group_by(dbio, species_common_name, species_science_name, year) %>% 
-    summarise(
-      n_age = sum(!is.na(age)),
-      n_length = sum(!is.na(length)),
-      n_weight = sum(!is.na(weight)),
-      n_maturity = sum(!is.na(maturity))
-    ) %>% ungroup
-  
-  if (!exists("dsurv")) {
-    dsurv <- readRDS("../../Dropbox/dfo/data/all-survey-dat.rds")
-    names(dsurv) <- tolower(names(dsurv))
-    dsurv$species_common_name <- tolower(dsurv$species_common_name)
-    dsurv$species_science_name <- tolower(dsurv$species_science_name)
-    dsurv <- mutate(dsurv, year = lubridate::year(trip_start_date))
-  }
-  
-  surv <- group_by(dsurv, species_common_name, species_science_name, 
-    survey_series_desc, year) %>% 
-    summarise(
-      n_fe_weight = sum(!is.na(catch_weight)),
-      n_fe_count = sum(!is.na(catch_count))
-    ) %>% ungroup
-  
-  surveys <- data.frame(survey_series_desc = c("Hecate Strait Synoptic Survey", 
-    "IPHC Longline Survey", 
-    "PHMA Rockfish Longline Survey - Outside North", 
-    "PHMA Rockfish Longline Survey - Outside South",
-    "Queen Charlotte Sound Synoptic Survey", 
-    "West Coast Haida Gwaii Synoptic Survey",
-    "West Coast Vancouver Island Synoptic Survey"),
-    surv_short = c("HS", "IPHC", "PHMAN", "PHMAS", "QCS", "WCHG", "WCVI"), 
-    stringsAsFactors = FALSE)
   
   sps <- filter(surv, species_common_name %in% common_name) %>% 
     select(-species_common_name, -species_science_name) %>% 
@@ -61,7 +69,8 @@ for (i in seq_along(torun)) {
     reshape2::dcast(year ~ surv_short, value.var = "combined") %>%
     right_join(data.frame(year = years))
   
-  # fix this!!!
+  # fix this!!! how ugly
+  # filling in zeros for missing surveys
   if (!"HS" %in% names(qq))  qq <- left_join(data.frame(HS = NA, year = years), qq)
   if (!"IPHC" %in% names(qq))  qq <- left_join(data.frame(IPHC = NA, year = years), qq)
   if (!"PHMAN" %in% names(qq))  qq <- left_join(data.frame(PHMAN = NA, year = years), qq)
@@ -69,11 +78,9 @@ for (i in seq_along(torun)) {
   if (!"QCS" %in% names(qq))  qq <- left_join(data.frame(QCS = NA, year = years), qq)
   if (!"WCHG" %in% names(qq))  qq <- left_join(data.frame(WCHG = NA, year = years), qq)
   if (!"WCVI" %in% names(qq))  qq <- left_join(data.frame(WCVI = NA, year = years), qq)
-  
   qq <- dplyr::select(qq, year, HS, IPHC, PHMAN, PHMAS, QCS, WCHG, WCVI)
-  # names(qq) <- gsub("PHMAS", "PHMA S", names(qq))
-  # names(qq) <- gsub("PHMAN", "PHMA N", names(qq))
-  
+  names(qq) <- gsub("PHMAS", "PHMA S", names(qq))
+  names(qq) <- gsub("PHMAN", "PHMA N", names(qq))
   for (i in 2:ncol(qq)) {
     qq[,i][is.na(qq[,i])] <- 0
   }
@@ -91,20 +98,10 @@ for (i in seq_along(torun)) {
     `# Age` = n_age
   )
   
-  ###############
-  years <- seq(1996, 2016)
-  labs <- intersect(seq(1800, 3000, 2), years)
-  # col <- viridis::viridis(200, option = "D")
-  col <- colorRampPalette(RColorBrewer::brewer.pal(9, "Blues"))(200)
-  # col <- colorRampPalette(RColorBrewer::brewer.pal(9, "Spectral"))(200)
-  grid_col <- "grey75"
-  box_col <- "grey55"
-  grid_lwd <- 1.3
-  surv_col <- "grey33"
-  axis_col <- "grey30"
-  ###############
+  #########
   
-  pdf(paste0("synop/dat-syn-", gsub("/", "-", gsub(" ", "-", common_name)), ".pdf"), width = 6, height = 3.5)
+  pdf(paste0("synop/dat-syn-", gsub("/", "-", gsub(" ", "-", common_name)), ".pdf"), 
+    width = 6, height = 3.5)
   layout(c(rep(1, 7), rep(2, 5)))
   par(mar = c(2.1, 5, .5, .5), cex = 0.8, oma = c(.5, .5, .5, .5))
   
