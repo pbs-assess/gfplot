6538eb6c49089c0e093b64a848135feaadce8c57
Sean Anderson <sean@seananderson.ca>
2017-10-27 16:04:35 -0700

    Tweaks

--- R/bio.R
+++ R/bio.R
@@ -24,12 +24,14 @@ if (!exists("dsurv")) {
   dsurv <- mutate(dsurv, year = lubridate::year(trip_start_date))
 }
 
+
 surv <- group_by(dsurv, species_common_name, species_science_name, 
   survey_series_desc, year) %>% 
   summarise(
     n_fe_weight = sum(!is.na(catch_weight)),
     n_fe_count = sum(!is.na(catch_count))
-  ) %>% ungroup
+  ) %>% ungroup %>% 
+  filter(!survey_series_desc %in% "Queen Charlotte Sound Shrimp Survey")
 
 surveys <- data.frame(survey_series_desc = c("Hecate Strait Synoptic Survey", 
   "IPHC Longline Survey", 
@@ -40,6 +42,9 @@ surveys <- data.frame(survey_series_desc = c("Hecate Strait Synoptic Survey",
   "West Coast Vancouver Island Synoptic Survey"),
   surv_short = c("HS", "IPHC", "PHMAN", "PHMAS", "QCS", "WCHG", "WCVI"), 
   stringsAsFactors = FALSE)
+
+all_surv_n <- group_by(surv, survey_series_desc, year) %>% 
+  summarise(n = sum(c(n_fe_weight, n_fe_count)))
 
 ###############
 years <- seq(1996, 2016)
@@ -54,7 +59,19 @@ surv_col <- "grey33"
 axis_col <- "grey30"
 ###############
 
-torun <- names(rev(sort(table(dbio$species_common_name)))[seq_len(50)])
+# when were surveys done?
+qq_all <- all_surv_n %>% 
+  right_join(expand.grid(year = years, 
+    survey_series_desc = unique(surv$survey_series_desc), stringsAsFactors = FALSE)) %>% 
+  mutate(present = ifelse(!is.na(n), TRUE, FALSE)) %>% 
+  select(-n) %>% ungroup() %>% 
+  left_join(surveys) %>% 
+  select(-survey_series_desc) %>% 
+  reshape2::dcast(year ~ surv_short, value.var = "present")
+
+######
+
+torun <- names(rev(sort(table(dbio$species_common_name)))[seq_len(1)])
 
 for (i in seq_along(torun)) {
   
@@ -79,10 +96,19 @@ for (i in seq_along(torun)) {
   if (!"WCHG" %in% names(qq))  qq <- left_join(data.frame(WCHG = NA, year = years), qq)
   if (!"WCVI" %in% names(qq))  qq <- left_join(data.frame(WCVI = NA, year = years), qq)
   qq <- dplyr::select(qq, year, HS, IPHC, PHMAN, PHMAS, QCS, WCHG, WCVI)
-  names(qq) <- gsub("PHMAS", "PHMA S", names(qq))
-  names(qq) <- gsub("PHMAN", "PHMA N", names(qq))
+  # names(qq) <- gsub("PHMAS", "PHMA S", names(qq))
+  # names(qq) <- gsub("PHMAN", "PHMA N", names(qq))
   for (i in 2:ncol(qq)) {
     qq[,i][is.na(qq[,i])] <- 0
+  }
+  
+  assertthat::assert_that(identical(names(qq_all), names(qq)))
+  qq_combined <- matrix(NA, nrow = nrow(qq), ncol = ncol(qq))
+  for (i in seq_along(qq$year)) {
+    for (j in seq(2, ncol(qq))) {
+      if(qq[i, j] > 0) qq_combined[i, j] <- 2
+      if(qq[i, j] == 0 & qq_all[i, j] == TRUE) qq_combined[i, j] <- 1
+    }
   }
   
   #########
@@ -103,23 +129,22 @@ for (i in seq_along(torun)) {
   pdf(paste0("synop/dat-syn-", gsub("/", "-", gsub(" ", "-", common_name)), ".pdf"), 
     width = 6, height = 3.5)
   layout(c(rep(1, 7), rep(2, 5)))
-  par(mar = c(2.1, 5, .5, .5), cex = 0.8, oma = c(.5, .5, .5, .5))
+  par(mar = c(2, 4.75, .5, .5), cex = 0.8, oma = c(0, .5, .5, .5))
   
-  image(x = qq$year, y = seq(1, ncol(qq) - 1), z = as.matrix(log(qq[, -1])), axes = FALSE,
-    xlab = "", ylab = "", col = surv_col)
-  axis(1, at = labs, labels = labs, tick = 0, line = -0.5, col.axis = axis_col)
+  image(x = qq$year, y = seq(1, ncol(qq) - 1), z = qq_combined[, -1], axes = FALSE,
+    xlab = "", ylab = "", col = c(grid_col, "grey25"))
+  axis(1, at = labs, labels = labs, tick = 0, line = -0.65, col.axis = axis_col)
   axis(2, at = seq(1, ncol(qq) - 1), labels = names(qq)[-1], las = 1, tick = 0, 
-    line = -0.2, col.axis = axis_col)
+    line = -0.4, col.axis = axis_col)
   abline(v = c(years, max(years)+1)-.5, col = grid_col, lwd = grid_lwd)
   abline(h = 1:ncol(qq)-.5, col = grid_col, lwd = grid_lwd)
   box(col = box_col, lwd = grid_lwd)
-  
-  
+
   image(x = spb$year, y = 1:4, z = as.matrix(log(spb[, -1])), axes = FALSE,
     xlab = "", ylab = "", col = col)
   
-  axis(1, at = labs, labels = labs, tick = 0, line = -0.5, col.axis = axis_col)
-  axis(2, at = 1:4, labels = names(spb)[-1], las = 1, tick = 0, line = -0.2,
+  axis(1, at = labs, labels = labs, tick = 0, line = -0.65, col.axis = axis_col)
+  axis(2, at = 1:4, labels = names(spb)[-1], las = 1, tick = 0, line = -0.4,
     col.axis = axis_col)
   abline(v = c(years, max(years)+1)-.5, col = grid_col, lwd = grid_lwd)
   abline(h = 1:5-.5, col = grid_col, lwd = grid_lwd)
@@ -133,8 +158,11 @@ for (i in seq_along(torun)) {
   }
   max_ind <- which(spb[,-1] == max_val, arr.ind = TRUE)
   max_ind <- max_ind[1,,drop = FALSE]
-  text(spb$year[max_ind[,"row"][[1]]], max_ind[,"col"][[1]], 
+  text(spb$year[max_ind[,"row"][[1]]], max_ind[,"col"][[1]],
     plyr::round_any(max_val, 100), col = "white", cex = 0.6)
+  
+  # for (i in seq(1, ncol(qq) - 1))
+  # text(spb$year, i, plyr::round_any(qq[,i], 100), col = "white", cex = 0.6)
   
   dev.off()
   
