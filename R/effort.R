ccd188f49e69c72c5b03bf3779e08a19f29d80a0
Sean Anderson <sean@seananderson.ca>
2017-12-13 17:15:47 -0800

    Strip whitespace

--- R/effort.R
+++ R/effort.R
@@ -6,7 +6,7 @@ library(ggplot2)
 source("R/make-spp-list.R")
 torun <- get_spp_names()
 
-d <- filter(d, species_common_name %in% 
+d <- filter(d, species_common_name %in%
     filter(torun, type == "A")$species_common_name)
 
 d <- mutate(d, gear = dplyr::recode(gear,
@@ -18,12 +18,13 @@ d <- mutate(d, gear = dplyr::recode(gear,
   `UNKNOWN TRAWL` = "Unknown/trawl"))
 
 dat <- filter(d, year >= 1996)
-dat <- dat %>% 
-  filter(fishery_sector == "GROUNDFISH TRAWL") %>% 
-  mutate(fe_time = 
-      as.numeric(difftime(fe_end_date, fe_start_date, units = "hours"))) %>% 
-  group_by(species_common_name, year) %>% 
-  summarise(total_hours = sum(fe_time, na.rm = TRUE)) %>% 
+# dat <- d
+dat <- dat %>%
+  filter(fishery_sector == "GROUNDFISH TRAWL") %>%
+  mutate(fe_time =
+      as.numeric(difftime(fe_end_date, fe_start_date, units = "hours"))) %>%
+  group_by(species_common_name, year) %>%
+  summarise(total_hours = sum(fe_time, na.rm = TRUE)) %>%
   ungroup()
 
 ggplot(dat, aes(year, total_hours)) +
