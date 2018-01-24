6de86be27013fbb7ed386137c4ad8168602e7ccc
Elise Keppel <Elise.Keppel@dfo-mpo.gc.ca>
2018-01-23 16:07:55 -0800

    updated maturity.R

--- R/maturity.R
+++ R/maturity.R
@@ -1,18 +1,16 @@
 library(dplyr)
+library(rio)
+
 dbio <- readRDS("data-cache/all-survey-bio.rds")
 dbio <- dbio[!duplicated(dbio$specimen_id), ]
 dbio <- dbio %>%
   select(species_common_name, species_science_name,
-    year, age, length, weight,
-    maturity_code, sex, survey_series_desc,
-    maturity_convention_desc, maturity_convention_maxvalue)
+         year, age, length, weight,
+         maturity_code, sex, survey_series_desc,
+         maturity_convention_desc, maturity_convention_maxvalue)
 
-mat_df <- tibble::tribble(
-  ~maturity_convention_desc, ~mature_at,
-  "ROCKFISH (1977+)",        3,
-  "DOGFISH",                 77,
-  "FLATFISH (1978+)",        3
-)
+mat_df <- import("data/maturity_assignment.csv") %>%
+  rename(sex = specimen_sex_code, maturity_convention_desc = maturity_convention_description)
 
 dbio <- left_join(dbio, mat_df)
 dbio <- mutate(dbio, mature = maturity_code >= mature_at)
