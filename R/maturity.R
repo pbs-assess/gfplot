5de1cc37e180594ce507b2daa5fca75960f5aec9
Sean Anderson <sean@seananderson.ca>
2018-01-24 11:50:13 -0800

    rio -> read_csv, indentation

--- R/maturity.R
+++ R/maturity.R
@@ -1,16 +1,16 @@
 library(dplyr)
-library(rio)
 
 dbio <- readRDS("data-cache/all-survey-bio.rds")
 dbio <- dbio[!duplicated(dbio$specimen_id), ]
 dbio <- dbio %>%
   select(species_common_name, species_science_name,
-         year, age, length, weight,
-         maturity_code, sex, survey_series_desc,
-         maturity_convention_desc, maturity_convention_maxvalue)
+    year, age, length, weight,
+    maturity_code, sex, survey_series_desc,
+    maturity_convention_desc, maturity_convention_maxvalue)
 
-mat_df <- import("data/maturity_assignment.csv") %>%
-  rename(sex = specimen_sex_code, maturity_convention_desc = maturity_convention_description)
+mat_df <- read_csv("data/maturity_assignment.csv") %>%
+  rename(sex = specimen_sex_code,
+    maturity_convention_desc = maturity_convention_description)
 
 dbio <- left_join(dbio, mat_df)
 dbio <- mutate(dbio, mature = maturity_code >= mature_at)
@@ -79,10 +79,10 @@ axis(2, las = 1, col = "grey60", col.axis = "grey40", col.ticks = "grey70")
 box(col = "grey60")
 
 # polygon(c(nd$length, rev(nd$length)), c(nd$lwr, rev(nd$upr)),
-  # col = "#00000020", border = NA)
+# col = "#00000020", border = NA)
 # points(xx$length,
-  # jitter(as.numeric(xx$mature), amount = 0.01),
-  # col = "#00000002", pch = 19, cex = 0.7)
+# jitter(as.numeric(xx$mature), amount = 0.01),
+# col = "#00000002", pch = 19, cex = 0.7)
 summary(m)
 
 # points(p$mean_length, p$p)
@@ -99,7 +99,7 @@ abline(v = l05, lty = 2, col = "grey40", lwd = 1.5)
 # abline(h = 0.95, lty = 2, col = "grey30", lwd = 1.5)
 
 # rect(xleft = l50$lwr, xright = l50$upr, ybottom = 0, ytop = 1,
-  # border = NA, col = "#00000040")
+# border = NA, col = "#00000040")
 lines(nd$length, nd$est, col = "red", lwd = 2.2)
 par(xpd = NA)
 symbols(p$mean_length, y = p$p,
