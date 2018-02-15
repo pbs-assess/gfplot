d8a0ec3c1ae896f91f382b41e0b03f5e447203a5
Sean Anderson <sean@seananderson.ca>
2018-02-14 23:07:13 -0800

    Finish growth funcs

--- R/growth.R
+++ R/growth.R
@@ -48,7 +48,7 @@ fit_vb <- function(dat,
   cores = parallel::detectCores(),
   allow_slow_mcmc = FALSE,
   est_method = median,
-  min_samples = 100L,
+  min_samples = 50L,
   ...) {
 
   if ("species_common_name" %in% names(dat))
@@ -62,7 +62,8 @@ fit_vb <- function(dat,
 
   dat <- switch(sex[[1]],
     "female" = filter(dat, sex == 2),
-    "male" = filter(dat, sex == 1))
+    "male" = filter(dat, sex == 1),
+    stop("`sex` argument must be 'female' or 'male'.", call. = FALSE))
 
   if (nrow(dat) < min_samples) {
     return(list(
@@ -123,11 +124,82 @@ fit_vb <- function(dat,
     pars = pars, data = as_tibble(dat), model = m)
 }
 
+#' Fit length-weight TODO
+#'
+#' For use with data for a single species.
+#'
+#' @param dat TODO
+#' @param sex TODO
+#' @param downsample TODO
+#' @param min_samples TODO
+#' @param method TODO
+#' @param scale_weight TODO
+#'
+#' @family growth functions
+#' @export
+# TODO:
+# @examples
+# d <- readRDS("data-cache/pbs-survey-specimens.rds")
+# d <- dplyr::filter(d, species_common_name == "pacific ocean perch")
+# obj_f <- fit_length_weight(d)
+# obj_f$model
+# obj_f$predictions
+# obj_f$pars
+# obj_f$data
+#
+# obj_m <- fit_length_weight(d, sex = "male")
+# plot_length_weight(obj_f, obj_m)
 
-#' Plot growth TODO
+fit_length_weight <- function(dat,
+  sex = c("female", "male"),
+  downsample = Inf,
+  min_samples = 50L,
+  method = c("rlm", "lm"),
+  scale_weight = 1/1000) {
+
+  if ("species_common_name" %in% names(dat))
+    if (length(unique(dat$species_common_name)) != 1L)
+      stop("Multiple species detected via the `species_common_name` column. ",
+        "fit_vb() is for use with a single species. Filter the data yourself ",
+        "first.", call. = FALSE)
+
+  dat <- dat[!duplicated(dat), , drop = FALSE]
+  dat <- filter(dat, !is.na(.data$sex), !is.na(.data$length), !is.na(.data$weight))
+
+  dat <- switch(sex[[1]],
+    "female" = filter(dat, sex == 2),
+    "male" = filter(dat, sex == 1),
+    stop("`sex` argument must be 'female' or 'male'.", call. = FALSE))
+
+  dat$weight <- dat$weight * scale_weight
+
+  if (nrow(dat) < min_samples) {
+    return(list(
+      predictions = tibble(length = NA, weight = NA),
+      pars = list(log_a = NA, b = NA),
+      data = dat,
+      model = NA))
+  }
+
+  m <- switch(method[[1]],
+    "rlm" = MASS::rlm(log(weight) ~ log(length), data = dat),
+    "lm" = stats::lm(log(weight) ~ log(length), data = dat),
+    stop("`method` argument must be 'rlm' or 'lm'.", call. = FALSE))
+
+  pred <- tibble(length = seq(min(dat$length), max(dat$length), length.out = 200L))
+  pred$weight <- exp(predict(m, newdata = pred))
+
+  pars <- as.list(coef(m))
+  pars <- stats::setNames(pars, c("log_a", "b"))
+
+  list(predictions = pred, pars = pars, data = as_tibble(dat), model = m)
+}
+
+#' Plot VB or length-weight fits TODO
 #'
 #' @param object_female TODO
 #' @param object_male TODO
+#' @param type TODO
 #' @param downsample TODO
 #' @param pt_alpha TODO
 #' @param xlab TODO
@@ -141,19 +213,32 @@ fit_vb <- function(dat,
 #'
 #' @export
 #' @family growth functions
+#' @rdname plot_growth
 #'
 # @examples
-plot_vb <- function(object_female, object_male, downsample = 2000L,
-  pt_alpha = 0.2, xlab = "Age (years)", ylab = "Length (cm)",
-  seed = 42, lab_x = 0.4, lab_y = 0.3, lab_x_gap = 0.3, lab_y_gap = 0.08,
+
+plot_growth <- function(object_female, object_male,
+  type = c("vb", "length-weight"),
+  downsample = 2000L,
+  pt_alpha = 0.2,
+  xlab = "Age (years)",
+  ylab = "Length (cm)",
+  seed = 42,
+  lab_x = 0.4,
+  lab_y = 0.3,
+  lab_x_gap = 0.3,
+  lab_y_gap = 0.08,
   col = c("Female" = "red", "Male" = "black")) {
+
+  xvar <- if (type[[1]] == "vb") "age" else "length"
+  yvar <- if (type[[1]] == "vb") "length" else "weight"
 
   line_dat <- bind_rows(
     data.frame(object_female$predictions, sex = "Female",
       stringsAsFactors = FALSE),
     data.frame(object_male$predictions, sex = "Male",
       stringsAsFactors = FALSE))
-  no_lines <- if (all(is.na(line_dat$age))) TRUE else FALSE
+  no_lines <- if (all(is.na(line_dat[[xvar]]))) TRUE else FALSE
 
   pt_dat <- bind_rows(object_female$data, object_male$data)
   pt_dat$sex <- dplyr::case_when(
@@ -161,6 +246,9 @@ plot_vb <- function(object_female, object_male, downsample = 2000L,
     pt_dat$sex == 2 ~ "Female",
     TRUE ~ "")
   no_pts <- if (nrow(pt_dat) == 0L) TRUE else FALSE
+
+  xdat <- if (type[[1]] == "vb") pt_dat$age else pt_dat$length
+  ydat <- if (type[[1]] == "vb") pt_dat$length else pt_dat$weight
 
   if (nrow(pt_dat) > downsample) {
     if (!is.null(seed)) set.seed(seed)
@@ -173,39 +261,67 @@ plot_vb <- function(object_female, object_male, downsample = 2000L,
     guides(colour = FALSE)
 
   if (!no_pts)
-    g <- g + geom_point(data = pt_dat, aes_string("age", "length"),
+    g <- g + geom_point(data = pt_dat, aes_string(xvar, yvar),
       alpha = pt_alpha, colour = "grey50") +
     coord_cartesian(
-      xlim = range(pt_dat$age),
-      ylim = c(0, max(pt_dat$length)), expand = FALSE)
+      xlim = range(xdat),
+      ylim = c(0, max(ydat)),
+      expand = FALSE)
 
   if (!no_lines)
-    g <- g + geom_line(data = line_dat, aes_string("age", "length",
+    g <- g + geom_line(data = line_dat, aes_string(xvar, yvar,
       colour = "sex"), size = 1)
 
-  if (!is.na(object_female$pars$linf)) {
-    g <- ann_vb(g, object_female$pars, "Females", col[["Female"]],
-      x = lab_x * max(pt_dat$age),
-      y = lab_y * max(pt_dat$length),
-      gap = lab_y_gap * max(pt_dat$length))
+  ann_func <- if (type[[1]] == "vb") ann_vb else ann_lw
+  if (!is.na(object_female$pars[[1]])) {
+    g <- ann_func(g, object_female$pars, "Females", col[["Female"]],
+      x = lab_x * max(xdat),
+      y = lab_y * max(ydat),
+      gap = lab_y_gap * max(ydat))
   }
 
-  if (!is.na(object_male$pars$linf)) {
-    g <- ann_vb(g, object_male$pars, "Males", col[["Male"]],
-      x = (lab_x + lab_x_gap) * max(pt_dat$age),
-      y = lab_y * max(pt_dat$length),
-      gap = lab_y_gap * max(pt_dat$length))
+  if (!is.na(object_male$pars[[1]])) {
+    g <- ann_func(g, object_male$pars, "Males", col[["Male"]],
+      x = (lab_x + lab_x_gap) * max(xdat),
+      y = lab_y * max(ydat),
+      gap = lab_y_gap * max(ydat))
   }
 
   g
 }
 
+#' @inheritParams plot_growth
+#' @param ... TODO
+#' @export
+#' @rdname plot_growth
+plot_vb <- function(..., type = "vb") {
+  plot_growth(..., type = type)
+}
+
+#' @inheritParams plot_vb
+#' @inheritParams plot_growth
+#' @export
+#' @rdname plot_growth
+plot_length_weight <- function(..., type = "length-weight", xlab = "Length (cm)",
+    ylab = "Weight (kg)", lab_x = 0.1, lab_y = 0.9, lab_x_gap = 0.35) {
+  plot_growth(..., type = type, xlab = xlab,
+    ylab = ylab, lab_x = lab_x, lab_y = lab_y, lab_x_gap = lab_x_gap)
+}
+
+# annotation helpers:
 ann_vb <- function(gg, pars, title, col, x, y, gap) {
   gg + ggplot2::annotate("text", label = title,
     x = x, y = y, hjust = 0, col = col) +
     ann("k", pars[["k"]], dec = 2, x, y - gap) +
     ann("linf", pars[["linf"]], dec = 1, x, y - gap * 2) +
     ann("t0", pars[["t0"]], dec = 2, x, y - gap * 3)
+}
+
+ann_lw <- function(gg, pars, title, col, x, y, gap) {
+  gg + ggplot2::annotate("text", label = title,
+    x = x, y = y, hjust = 0, col = col) +
+    ann("log(a)", pars[["log_a"]], dec = 2, x, y - gap) +
+    ann("b", pars[["b"]], dec = 2, x, y - gap * 2)
 }
 
 ann <- function(par_name, par_val, dec, x, y, col = "grey10") {
