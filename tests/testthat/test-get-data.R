2517b3fccdc89a2dc6077d68da4fb5cf12504abf
elisekeppel <elisekeppel@dfo-mpo.gc.ca>
2018-02-21 16:43:28 -0800

    adjust get-comm and get-surv-samples.sql

--- tests/testthat/test-get-data.R
+++ tests/testthat/test-get-data.R
@@ -5,17 +5,26 @@ test_that("get_* data functions work at PBS", {
   skip_on_appveyor()
 
   if (!is_dfo_windows()) skip("Not a DFO computer")
-  d <- get_age_precision("lingcod")
-  expect_equal(d$species_code[[1L]], "467")
+  # d <- get_age_precision("lingcod")
+  # expect_equal(d$species_code[[1L]], "467")
+  #
+  # d <- get_catch("lingcod")
+  # expect_gte(nrow(d), 1L)
+  #
+  # d <- get_surv_tows("lingcod", 1)
+  # expect_gte(d$catch_weight[[1]], 0)
+  #
+  # d <- get_surv_samples("lingcod", 16)
+  # expect_gte(d$survey_id[[1]], 1L)
 
-  d <- get_catch("lingcod")
-  expect_gte(nrow(d), 1L)
+  # d <- get_comm_samples("lingcod")
+  # expect_gte(d$year[[1]], 1900L)
 
-  d <- get_surv_tows("lingcod", 1)
-  expect_gte(d$catch_weight[[1]], 1L)
+  d <- get_cpue_spatial("lingcod")
+  expect_false(is.null(d$lat[[1]]))
+  expect_false(is.null(d$lon[[1]]))
 
-  d <- get_surv_samples("lingcod", 16)
-  expect_gte(d$survey_id[[1]], 1L)
-
+  # d <- get_comm_samples("lingcod")
+  # expect_gte(d$year[[1]], 1900L)
 
 })
