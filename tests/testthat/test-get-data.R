13eb2749eacfe822bf4f8ad29482fc57d927de07
elisekeppel <elisekeppel@dfo-mpo.gc.ca>
2018-02-22 17:23:00 -0800

    testing get-dat.R

--- tests/testthat/test-get-data.R
+++ tests/testthat/test-get-data.R
@@ -5,36 +5,52 @@ test_that("get_* data functions work at PBS", {
   skip_on_appveyor()
 
   if (!is_dfo_windows()) skip("Not a DFO computer")
-  # d <- get_age_precision("lingcod")
-  # expect_equal(d$species_code[[1L]], "467")
-  #
-  # d <- get_catch("lingcod")
-  # expect_gte(nrow(d), 1L)
-  #
-  # d <- get_survey_tows("lingcod", 1)
-  # expect_gte(d$catch_weight[[1]], 0)
-  #
-  # d <- get_survey_samples("lingcod", 16)
-  # expect_gte(d$survey_id[[1]], 1L)
+  d <- get_age_precision("lingcod")
+  expect_equal(d$species_code[[1L]], "467")
 
-  # d <- get_comm_samples("lingcod")
-  # expect_gte(d$year[[1]], 1900L)
+  d <- get_catch("lingcod")
+  expect_gte(nrow(d), 1L)
 
-  # d <- get_cpue_spatial("lingcod")
-  # expect_false(is.null(d$lat[[1]]))
-  # expect_false(is.null(d$lon[[1]]))
+  d <- get_survey_tows("lingcod", 1)
+  expect_gte(d$catch_weight[[1]], 0)
 
-  # d <- get_cpue_spatial_ll("lingcod")
-  # expect_type(d$fishing_event_id, "integer")
+  d <- get_survey_samples("lingcod", 16)
+  expect_gte(d$survey_id[[1]], 1L)
 
-  # d <- get_cpue_index("bottom trawl", min_year = 2015)
-  # expect_true(!is.null(d$fe_start_date))
+  d <- get_comm_samples("lingcod")
+  expect_gte(d$year[[1]], 1900L)
 
-  # Why doesn't this work?
+  d <- get_cpue_spatial("lingcod")
+  expect_false(is.null(d$lat[[1]]))
+  expect_false(is.null(d$lon[[1]]))
+
+  d <- get_cpue_spatial_ll("lingcod")
+  expect_type(d$fishing_event_id, "integer")
+
+  d <- get_cpue_index("bottom trawl", min_year = 2015)
+  expect_true(!is.null(d$fe_start_date))
+
+  # Why doesn't this work? Error says that best_date is of
+  # type "double", but it is infact "datetime" in function output
   # d <- get_cpue_index("bottom trawl", min_year = 2015)
   # expect_type(d$best_date, "datetime")
 
-  d <- get_surv_index("lingcod", ssid = 1)
+  d <- get_survey_index("lingcod", ssid = 1)
   expect_type(d$num_pos_sets, "integer")
+
+  d <- get_ssids()
+  expect_type(d$survey_series_desc, "character")
+
+  d <- get_age_methods()
+  expect_true(!is.null(d$ageing_method_desc))
+
+  d <- get_sample_trips()
+  expect_true(!is.null(d$SAMPLE_ID[[1]]))
+
+  d <- get_strata_areas()
+  expect_true(!is.null(d$SURVEY_ID[[1]]))
+
+  d <- get_survey_ids(16)
+  expect_true(!is.null(d$SURVEY_ID[[1]]))
 
 })
