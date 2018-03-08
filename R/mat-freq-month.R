7a96e46fc8a32036e9eb4a17a6fa4f99be77b38f
elisekeppel <elisekeppel@dfo-mpo.gc.ca>
2018-03-07 16:59:13 -0800

    joined mat short names csv into plot mat freq

--- R/mat-freq-month.R
+++ R/mat-freq-month.R
@@ -16,32 +16,45 @@ tidy_maturity_months <- function(dat, months = seq(1, 12),
   dat <- dat %>%
     select(species_common_name,
       month,
+      maturity_convention_code,
       maturity_code,
       maturity_name,
       sex)
 
   dat <- filter(dat, !is.na(maturity_name), !is.na(sex))
 
+  file <- system.file("extdata", "maturity_short_names.csv",
+    package = "gfplot")
+
+  mat_df <- readr::read_csv(file,
+    col_types = readr::cols(
+      maturity_convention_code = readr::col_integer(),
+      maturity_code = readr::col_integer(),
+      sex = readr::col_integer(),
+      maturity_name_short = readr::col_character()))
+
+  dat <- left_join(dat, mat_df, by = c("sex", "maturity_convention_code", "maturity_code"))
+
   dat <- dat %>% mutate(sex = ifelse(sex == 2, "F", "M"))
 
-  dat <- dat %>%
-    mutate(maturity_name_short = replace(maturity_name, maturity_name %in%
-        c("IMMATURE-1", "IMMATURE-2", "MATURING-SMALL", "MATURING - SMALL"),
-      "Immature")) %>%
-    mutate(maturity_name_short = replace(maturity_name_short,
-      maturity_name_short %in%
-        c("MATURING-LARGE", "PRE-RIPENING", "PRE-RIPENING-R1",
-          "PRE-RIPENING-R2", "RIPENING", "RIPENING-2R", "MATURING - LARGE"),
-      "Mature")) %>%
-    mutate(maturity_name_short = replace(maturity_name_short,
-      maturity_name_short %in% c("MATURE (SPENT)"), "Spent")) %>%
-    mutate(maturity_name_short = replace(maturity_name_short,
-      maturity_name_short %in% c("MATURE (RIPE)"), "Ripe")) %>%
-    mutate(maturity_name_short = replace(maturity_name_short,
-      maturity_name_short %in% c("MATURE (RESTING)"), "Resting"))
+  # dat <- dat %>%
+  #   mutate(maturity_name_short = replace(maturity_name, maturity_name %in%
+  #       c("IMMATURE-1", "IMMATURE-2", "MATURING-SMALL", "MATURING - SMALL"),
+  #     "Immature")) %>%
+  #   mutate(maturity_name_short = replace(maturity_name_short,
+  #     maturity_name_short %in%
+  #       c("MATURING-LARGE", "PRE-RIPENING", "PRE-RIPENING-R1",
+  #         "PRE-RIPENING-R2", "RIPENING", "RIPENING-2R", "MATURING - LARGE"),
+  #     "Mature")) %>%
+  #   mutate(maturity_name_short = replace(maturity_name_short,
+  #     maturity_name_short %in% c("MATURE (SPENT)"), "Spent")) %>%
+  #   mutate(maturity_name_short = replace(maturity_name_short,
+  #     maturity_name_short %in% c("MATURE (RIPE)"), "Ripe")) %>%
+  #   mutate(maturity_name_short = replace(maturity_name_short,
+  #     maturity_name_short %in% c("MATURE (RESTING)"), "Resting"))
 
-  mat_levels <- rev(c("Immature", "Mature", "Ripe", "Spent", "Resting"))
-  dat$maturity_name_short <- stringr::str_to_title(dat$maturity_name_short)
+  mat_levels <- rev(c("Immature", "Maturing", "Maturing-Small", "Maturing-Large", "Pre-Ripening (Mature)", "Mature", "Ripening (Mature)", "Gravid", "Fertilized", "Ripe", "Running Ripe", "Embryos", "Spent", "Resorbing", "Recovering", "Resting"))
+  # dat$maturity_name_short <- stringr::str_to_title(dat$maturity_name_short)
   dat$maturity_name_short <- factor(dat$maturity_name_short,
     levels = mat_levels)
 
