db71c60256544181bb2eec26b0ce6d17f29f7556
Sean Anderson <sean@seananderson.ca>
2018-03-14 16:16:10 -0700

    Add bind_samples

---
+++ R/bind_samples.R
@@ -0,0 +1,13 @@
+#' Join survey and commercial samples
+#'
+#' @param dat_comm TODO
+#' @param dat_survey TODO
+#' @export
+
+bind_samples <- function(dat_comm, dat_survey) {
+  dat_comm$data_source <- "commercial"
+  dat_comm$data_source <- "survey"
+  inter <- intersect(names(dat_comm), names(dat_survey))
+  dplyr::bind_rows(dat_comm[ , inter, drop = FALSE],
+    dat_survey[, inter, drop = FALSE])
+}
