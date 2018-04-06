66554946bb9299ae9a0ff7c50528f928e5661408
Sean Anderson <sean@seananderson.ca>
2018-04-06 12:36:30 -0700

    dat_comm -> dat_survey bug

--- R/bind-samples.R
+++ R/bind-samples.R
@@ -12,7 +12,7 @@
 
 bind_samples <- function(dat_comm, dat_survey) {
   dat_comm$data_source <- "commercial"
-  dat_comm$data_source <- "survey"
+  dat_survey$data_source <- "survey"
   inter <- intersect(names(dat_comm), names(dat_survey))
   dplyr::bind_rows(
     dat_comm[, inter, drop = FALSE],
