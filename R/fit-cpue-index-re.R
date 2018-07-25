45148f25f1e253072c0670402dba295fd4852c63
Sean Anderson <sean@seananderson.ca>
2018-07-24 17:55:22 -0700

    Add some mixed effect CPUE formula parsing magic

---
+++ R/fit-cpue-index-re.R
@@ -0,0 +1,145 @@
+# Random effect version:
+fct_to_tmb_num <- function(x) {
+  as.numeric(as.factor(as.character(x))) - 1
+}
+
+load_tmb_2re_cpue <- function() {
+  tmb_cpp_2re <- system.file("tmb", "deltalognormal_2re.cpp", package = "gfplot")
+  TMB::compile(tmb_cpp_2re)
+  dyn.load(TMB::dynlib(sub("\\.cpp", "", tmb_cpp_2re)))
+}
+
+load_tmb_1re_cpue <- function() {
+  tmb_cpp_1re <- system.file("tmb", "deltalognormal_1re.cpp", package = "gfplot")
+  TMB::compile(tmb_cpp_1re)
+  dyn.load(TMB::dynlib(sub("\\.cpp", "", tmb_cpp_1re)))
+}
+
+parse_formula <- function(f) {
+  terms_ <- attributes(terms(f))$term.labels
+  re <- terms_[grepl("1 \\|", terms_)]
+  re <- gsub("1 \\| ", "", re)
+  fe <- terms_[!grepl("1 \\|", terms_)]
+  response <- all.vars(terms(formula_binomial))[1]
+  fe <- as.formula(paste(response, "~", paste(fe, collapse = " + ")))
+  list(response = response, fe = fe, re = re)
+}
+
+fit_cpue_index_re <- function(dat,
+                              formula_binomial = pos_catch ~ year_factor,
+                              formula_lognormal = log(spp_catch / hours_fished) ~ year_factor) {
+
+  f_bin <- parse_formula(formula_binomial)
+  f_pos <- parse_formula(formula_lognormal)
+
+  if (!identical(f_bin$re, f_pos$re))
+    stop("Random effects for the binomial and positive components must be identical.")
+
+  re1 <- NULL
+  re2 <- NULL
+  if (length(f_bin$re) == 1L) {
+    re1 <- f_bin$re[[1]]
+  }
+  if (length(f_bin$re) == 2L) {
+    re1 <- f_bin$re[[1]]
+    re2 <- f_bin$re[[2]]
+  }
+
+  pos_dat <- dat[dat$pos_catch == 1, , drop = FALSE]
+
+  if (!is.null(re1)) {
+    bin_re_id_k <- fct_to_tmb_num(dat[[re1]])
+    pos_re_id_k <- fct_to_tmb_num(filter(dat, pos_catch == 1)[[re1]])
+  }
+  if (!is.null(re2)) {
+    bin_re_id_g <- fct_to_tmb_num(dat[[re2]])
+    pos_re_id_g <- fct_to_tmb_num(filter(dat, pos_catch == 1)[[re2]])
+}
+  mm1 <- model.matrix(f_bin$fe, data = dat)
+  mm2 <- model.matrix(f_pos$fe, data = pos_dat)
+  mm_pred1 <- make_pred_mm(mm1, years = unique(pos_dat$year_factor))
+  mm_pred2 <- make_pred_mm(mm2, years = unique(dat$year_factor))
+
+  dlls <- getLoadedDLLs()
+  if (!is.null(re1) && !is.null(re2) && !any(vapply(dlls, function(x)
+    x[["name"]] == "deltalognormal_2re", FUN.VALUE = TRUE))) {
+    load_tmb_2re_cpue()
+  }
+  if (!is.null(re1) && is.null(re2) && !any(vapply(dlls, function(x)
+    x[["name"]] == "deltalognormal_1re", FUN.VALUE = TRUE))) {
+    load_tmb_1re_cpue()
+  }
+  if (is.null(re1) && is.null(re2) && !any(vapply(dlls, function(x)
+    x[["name"]] == "deltalognormal", FUN.VALUE = TRUE))) {
+    load_tmb_cpue()
+  }
+
+  # defaults if no random effects:
+  random <- NULL
+  DLL <- "deltalognormal"
+
+  parameters <- list(
+    b1_j = stats::rnorm(ncol(mm1), 0, 0.5),
+    b2_j = stats::rnorm(ncol(mm2), 0, 0.5),
+    log_sigma = log(1.0))
+
+  data <- list(
+    X1_ij = mm1,
+    y1_i = dat$pos_catch,
+    X2_ij = mm2,
+    y2_i = log(pos_dat$spp_catch / pos_dat$hours_fished),
+    X1_pred_ij = mm_pred1,
+    X2_pred_ij = mm_pred2
+  )
+
+  if (!is.null(re1)) {
+    data$factor1k_i <- bin_re_id_k
+    data$factor2k_i <- pos_re_id_k
+    data$nk1 <- length(unique(bin_re_id_k))
+    data$nk2 <- length(unique(pos_re_id_k))
+    random <- c("z1_k", "z2_k")
+    DLL <- "deltalognormal_1re"
+    parameters$z1_k <- stats::rnorm(length(unique(bin_re_id_k)), 0, 0.2)
+    parameters$z2_k <- stats::rnorm(length(unique(pos_re_id_k)), 0, 0.2)
+    parameters$log_sigma_zk1 <- log(0.2)
+    parameters$log_sigma_zk2 <- log(0.2)
+  }
+
+  if (!is.null(re2)) {
+    data$factor1g_i <- bin_re_id_g
+    data$factor2g_i <- pos_re_id_g
+    data$ng1 <- length(unique(bin_re_id_g))
+    data$ng2 <- length(unique(pos_re_id_g))
+    random <- c(random, "z1_g", "z2_g")
+    DLL <- "deltalognormal_2re"
+    parameters$z1_g <- stats::rnorm(length(unique(bin_re_id_g)), 0, 0.2)
+    parameters$z2_g <- stats::rnorm(length(unique(pos_re_id_g)), 0, 0.2)
+    parameters$log_sigma_zg1 <- log(0.2)
+    parameters$log_sigma_zg2 <- log(0.2)
+  }
+
+  obj <- TMB::MakeADFun(
+    data = data,
+    parameters = parameters,
+    random = random,
+    DLL = DLL,
+    checkParameterOrder = FALSE
+  )
+
+  opt <- stats::nlminb(
+    start = obj$par,
+    objective = obj$fn,
+    gradient = obj$gr,
+    control = list(iter.max = 1000L, eval.max = 1000L)
+  )
+
+  message("Getting TMB::sdreport()")
+  r <- TMB::sdreport(obj)
+
+  list(
+    model = opt, sdreport = r, max_gradient = max(obj$gr(opt$par)),
+    years = sort(unique(dat$year)), mm_bin = mm1, mm_pos = mm2,
+    f_bin = formula_binomial, f_pos = formula_lognormal,
+    data = dat
+  )
+}
--- inst/tmb/deltalognormal_1re.cpp
+++ inst/tmb/deltalognormal_1re.cpp
@@ -12,25 +12,15 @@ Type objective_function<Type>::operator() ()
   DATA_INTEGER(nk1);
   DATA_INTEGER(nk2);
 
-  DATA_IVECTOR(factor1g_i);
-  DATA_IVECTOR(factor2g_i);
-  DATA_INTEGER(ng1);
-  DATA_INTEGER(ng2);
-
-
   PARAMETER_VECTOR(b1_j);
   PARAMETER_VECTOR(b2_j);
 
   PARAMETER_VECTOR(z1_k);
   PARAMETER_VECTOR(z2_k);
-  PARAMETER_VECTOR(z1_g);
-  PARAMETER_VECTOR(z2_g);
 
   PARAMETER(log_sigma);
   PARAMETER(log_sigma_zk1);
   PARAMETER(log_sigma_zk2);
-  PARAMETER(log_sigma_zg1);
-  PARAMETER(log_sigma_zg2);
 
   DATA_MATRIX(X1_pred_ij);
   DATA_MATRIX(X2_pred_ij);
@@ -49,13 +39,13 @@ Type objective_function<Type>::operator() ()
   for(int i = 0; i < n1; i++){
     jnll -= dbinom_robust(y1_i(i), Type(1.0),
       linear_predictor1_i(i) +
-        z1_k(factor1k_i(i)) + z1_g(factor1g_i(i)),
+        z1_k(factor1k_i(i)),
       true);
   }
   for(int i = 0; i < n2; i++){
     jnll -= dnorm(y2_i(i),
       linear_predictor2_i(i) +
-        z2_k(factor2k_i(i)) + z2_g(factor2g_i(i)),
+        z2_k(factor2k_i(i)),
         exp(log_sigma),
         true);
   }
@@ -66,12 +56,6 @@ Type objective_function<Type>::operator() ()
   }
   for(int k = 0; k<nk2; k++){
     jnll -= dnorm(z2_k(k), Type(0.0), exp(log_sigma_zk2), true);
-  }
-  for(int g = 0; g<ng1; g++){
-    jnll -= dnorm(z1_g(g), Type(0.0), exp(log_sigma_zg1), true);
-  }
-  for(int g = 0; g<ng2; g++){
-    jnll -= dnorm(z2_g(g), Type(0.0), exp(log_sigma_zg2), true);
   }
 
   Type sigma = exp(log_sigma);
