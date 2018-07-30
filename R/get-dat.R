11fe32a84fad89e14bceffbd2e834af2002b2c3d
Sean Anderson <sean@seananderson.ca>
2018-07-30 16:39:56 -0700

    Filter before date mutating

--- R/get-dat.R
+++ R/get-dat.R
@@ -503,6 +503,12 @@ get_cpue_historic <- function(species,
   .d$gear <- tolower(.d$gear)
   .d$locality_description <- tolower(.d$locality_description)
 
+
+  # Filter out fishing records after last year required
+  if (!is.null(end_year)){
+    .d <- .d %>% filter(year <= end_year)
+  }
+
   # Create possibly alternate starting date:
   .d <- dplyr::mutate(.d, best_date = lubridate::ymd_hms(best_date))
   .d <- dplyr::mutate(.d, .year_start_date =
@@ -510,11 +516,6 @@ get_cpue_historic <- function(species,
   .d <- dplyr::mutate(.d, .time_diff = best_date - .year_start_date)
   .d <- dplyr::mutate(.d, alt_year = ifelse(.time_diff > 0, year, year - 1L))
   .d <- dplyr::select(.d, -.time_diff, -.year_start_date)
-
-  # Filter out fishing records after last year required
-  if (!is.null(end_year)){
-    .d <- .d %>% filter(year <= end_year)
-  }
 
   .d$area <- NA
   for (i in seq_along(areas)) {
