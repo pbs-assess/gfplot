b4322284482f3b814bc4bef9356039e904c9efff
Sean Anderson <sean@seananderson.ca>
2018-08-17 16:12:01 -0700

    Clean up extra plots

--- R/extra.R
+++ R/extra.R
@@ -1,29 +1,33 @@
 plot_cpue_predictions <- function(dat, model_version = "Combined", scale = FALSE) {
   if (scale) {
     dat <- dat %>%
-      group_by(formula_version, model, area) %>%
-      mutate(geo_mean = exp(mean(log(est)))) %>%
-      mutate(upr = upr / geo_mean, lwr = lwr / geo_mean, est = est / geo_mean) %>%
+      group_by(.data$formula_version, .data$model, .data$area) %>%
+      mutate(geo_mean = exp(mean(log(.data$est)))) %>%
+      mutate(upr = .data$upr / .data$geo_mean, lwr = .data$lwr / .data$geo_mean,
+        est = .data$est / .data$geo_mean) %>%
       ungroup()
   }
 
   unstandardized <- dat %>%
-    filter(formula_version == "Unstandardized") %>%
-    rename(est_unstandardized = est, lwr_unstandardized = lwr, upr_unstandardized = upr) %>%
-    select(year, area, model, est_unstandardized, lwr_unstandardized, upr_unstandardized)
+    filter(.data$formula_version == "Unstandardized") %>%
+    rename(est_unstandardized = .data$est,
+      lwr_unstandardized = .data$lwr, upr_unstandardized = .data$upr) %>%
+    select(.data$year, .data$area, .data$model,
+      .data$est_unstandardized, .data$lwr_unstandardized,
+      .data$upr_unstandardized)
 
   temp <- dat %>%
-    filter(formula_version != "Unstandardized") %>%
+    filter(ormula_version != "Unstandardized") %>%
     left_join(unstandardized, by = c("area", "year", "model")) %>%
     filter(model == model_version) %>%
-    mutate(formula_version = gsub("\\+ ", " ", formula_version)) %>%
+    mutate(formula_version = gsub("\\+ ", " ", .data$formula_version)) %>%
     mutate(
       formula_version =
-        gsub("Full standardization", "All variables", formula_version)
+        gsub("Full standardization", "All variables", .data$formula_version)
     ) %>%
     mutate(
       formula_version =
-        forcats::fct_relevel(formula_version, "All variables", after = Inf)
+        forcats::fct_relevel(.data$formula_version, "All variables", after = Inf)
     )
   temp %>%
     ggplot(aes_string("year", "est", ymin = "upr", ymax = "lwr")) +
@@ -44,58 +48,3 @@ plot_cpue_predictions <- function(dat, model_version = "Combined", scale = FALSE
     guides(colour = FALSE, fill = FALSE)
 }
 
-plot_predictor_bubbles <- function(dat, variable, reorder_group = FALSE,
-                              group_id = "fishing_event_id", ncol = 2) {
-  temp_pos <- dat %>%
-    filter(spp_catch > 0) %>%
-    group_by(area, year, !!rlang::sym(variable)) %>%
-    summarise(n = length(unique(!!rlang::sym(group_id)))) %>%
-    group_by(area, !!rlang::sym(variable)) %>%
-    mutate(n_tot = sum(n)) %>%
-    ungroup()
-
-  temp_all <- dat %>%
-    group_by(area, year, !!rlang::sym(variable)) %>%
-    summarise(n = length(unique(!!rlang::sym(group_id)))) %>%
-    group_by(area, !!rlang::sym(variable)) %>%
-    mutate(n_tot = sum(n)) %>%
-    ungroup()
-
-  if (reorder_group) {
-    # temp[, variable] <- paste(temp$area,
-    #   as.character(temp[, variable,drop=TRUE]))
-    # temp$y_group <- forcats::fct_reorder2(temp[, variable, drop=TRUE],
-    #   temp$area, -temp$n_tot)
-    temp_pos$y_group <- temp_pos[, variable, drop = TRUE]
-    temp_all$y_group <- temp_all[, variable, drop = TRUE]
-  } else {
-    temp_pos$y_group <- temp_pos[, variable, drop = TRUE]
-    temp_all$y_group <- temp_all[, variable, drop = TRUE]
-  }
-
-  temp_pos %>%
-    ggplot(aes_string("as.factor(year)", y = "y_group")) +
-    geom_point(aes_string(size = "n", fill = "n"), alpha = 0.4, pch = 21) +
-    geom_point(data = temp_all, aes_string(size = "n"), alpha = 0.4, pch = 21) +
-    facet_wrap(~area, scales = "free", ncol = ncol) +
-    scale_x_discrete(breaks = seq(1950, 2020, 10)) +
-    xlab("") + ylab(firstup(gsub("_", " ", variable))) +
-    labs(size = paste0("Number of\n", group_id)) +
-    labs(fill = paste0("Number of\n", group_id)) +
-    ggplot2::scale_size_continuous(range = c(0, 7)) +
-    ggplot2::scale_fill_viridis_c(trans = "log", breaks = c(1, 10, 100, 500))
-}
-
-plot_predictor_boxplots <- function(dat, y, ylab = "", title = "") {
-  ggplot(dat, aes_string("as.factor(year)", y = y)) +
-    geom_boxplot(outlier.size = 0.3, outlier.alpha = 0.5) +
-    ggplot2::scale_y_log10(
-      breaks = c(0.1, 1, 10, 100, 1000, 10000),
-      labels = scales::comma
-    ) +
-    ggplot2::scale_x_discrete(breaks = seq(1950, 2030, 5)) +
-    theme_pbs() +
-    theme(axis.text.x = element_text(angle = 0, vjust = 0.5)) +
-    facet_wrap(~area) + xlab("") + ylab(ylab) +
-    ggplot2::ggtitle(title)
-}
---
+++ R/plot_predictor_bubbles.R
@@ -0,0 +1,47 @@
+#' Make predictor level bubble plots
+#'
+#' @param dat A data frame with the columns area, year
+#' @param variable The column to summarize (character value)
+#' @param group_id The events to count over (e.g. `"fishing_event_id"` or `"trip_id"`)
+#' @param ncol Number of facetted columns (facets by area)
+#'
+#' @return A ggplot object
+#' @export
+#'
+#' @examples
+#' d <- data.frame(year = rep(1991:2010, each = 20),
+#'   spp_catch = rbinom(20 * 20, size = 1, prob = 0.8),
+#'   fishing_event_id = rep(1:20, 20),
+#'   my_predictor = sample(gl(20, 20), size = 400),
+#'   area = rep("a", 400))
+#' plot_predictor_bubbles(d, "my_predictor")
+plot_predictor_bubbles <- function(dat, variable,
+  group_id = "fishing_event_id", ncol = 2) {
+  temp_pos <- dat %>%
+    filter(spp_catch > 0) %>%
+    group_by(area, year, !!rlang::sym(variable)) %>%
+    summarise(n = length(unique(!!rlang::sym(group_id)))) %>%
+    group_by(area, !!rlang::sym(variable)) %>%
+    mutate(n_tot = sum(n)) %>%
+    ungroup()
+
+  temp_all <- dat %>%
+    group_by(area, year, !!rlang::sym(variable)) %>%
+    summarise(n = length(unique(!!rlang::sym(group_id)))) %>%
+    group_by(area, !!rlang::sym(variable)) %>%
+    mutate(n_tot = sum(n)) %>%
+    ungroup()
+
+  temp_pos %>%
+    ggplot(aes_string("as.factor(year)", y = variable)) +
+    geom_point(aes_string(size = "n", fill = "n"), alpha = 0.4, pch = 21) +
+    geom_point(data = temp_all, aes_string(size = "n"), alpha = 0.4, pch = 21) +
+    facet_wrap(~area, scales = "free", ncol = ncol) +
+    ggplot2::scale_x_discrete(breaks = seq(1950, 2020, 5)) +
+    xlab("") + ylab(firstup(gsub("_", " ", variable))) +
+    labs(size = paste0("Number of\n", group_id)) +
+    labs(fill = paste0("Number of\n", group_id)) +
+    ggplot2::scale_size_continuous(range = c(0, 7)) +
+    ggplot2::scale_fill_viridis_c(trans = "log", breaks = c(1, 10, 100, 500)) +
+    theme_pbs()
+}
