8f3a0303efe845c5ad7c7a1f876491d850f37648
Sean Anderson <sean@seananderson.ca>
2018-09-05 16:22:31 -0700

    Add mat_perc to ogive output

--- R/maturity.R
+++ R/maturity.R
@@ -123,9 +123,13 @@ fit_mat_ogive <- function(dat,
   nd$glmm_fe <- plogis(b[[1L]] + b[[3L]] * nd$female +
     b[[2L]] * nd$age_or_length + b[[4L]] * nd$age_or_length * nd$female)
 
+  mat_perc <- extract_maturity_perc(stats::coef(m))
+  se_l50 <- delta_method(~ -(log((1/0.5) - 1) + x1 + x3) / (x2 + x4),
+    mean = stats::coef(m), cov = stats::vcov(m))
+
   list(
     data = .d, pred_data = nd, model = m, sample_id_re = sample_id_re,
-    type = type
+    type = type, mat_perc = mat_perc, se_l50 = se_l50
   )
 }
 
