ee3c4dc1c65634f9282b407c87ec4181e6e32d9f
Sean Anderson <sean@seananderson.ca>
2018-10-12 16:28:19 -0700

    Update Makevars for Stan

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
 mkdir -p ~/.R
+
+echo "CXX14FLAGS=-O3 -mtune=native -march=native -Wno-unused-variable -Wno-unused-function  -Wno-macro-redefined" >> ~/.R/Makevars
 echo "CXX14 = g++ -fPIC"    >> ~/.R/Makevars
+echo "CXX14FLAGS+=-flto -Wno-unused-local-typedefs" >> ~/.R/Makevars
