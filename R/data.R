643f1f4d0d628523764d0b49e5de916f4d2e99ed
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-10-16 14:31:57 -0700

    Add .rda files for early IPHC data, plus descriptions.

--- R/data.R
+++ R/data.R
@@ -62,3 +62,76 @@
 #'
 #' @format A data frame
 "survey_boundaries"
+
+#' Station data for 1995 IPHC survey
+#'
+#' A dataset containing details of the Setline Grid stations for the 1995 IPHC survey, one set was deployed at each station.
+#'
+#' @format A tibble:
+#' \describe{
+#'   \item{lat}{latitude of station}
+#'   \item{lon}{longitude of station}
+#'   \item{station}{station name}
+#'   \item{effSkate}{effective skate number of the set, as calculated by IPHC}
+#'   \item{usable}{whether or not the set is usable, as determined by IPHC plus
+#'                 four extra that had no hooks enumerated except for Pacific
+#'                 Halibut, and so are not usable by us}
+#'
+#' }
+#' @source Originally from file 1995IPHCSetlineData.xlsx that Lynne Yamanaka
+#'         received from Aaron Ranta at the IPHC.
+"setData1995"
+
+#' Count data for 1995 IPHC survey
+#'
+#' A dataset containing counts of each species for each Setline Grid station for
+#' the 1995 IPHC survey.
+#'
+#' @format A tibble:
+#' \describe{
+#'   \item{year}{1995 for this data set}
+#'   \item{station}{station name, to link with setData1995}
+#'   \item{specName}{species name for the counts for that set}
+#'   \item{specCount}{count of that species for that set}
+#' }
+#' @source Originally from file 1995IPHCSetlineData.xlsx that Lynne Yamanaka
+#'         received from Aaron Ranta at the IPHC.
+"countData1995"
+
+#' Station data for 1995 IPHC survey for RS stations
+#'
+#' A dataset containing details of the RS (Random Sample?) stations for the 1995
+#' IPHC survey, one set was deployed at each station. We are not using these data
+#' but including here for completeness.
+#'
+#' @format A tibble:
+#' \describe{
+#'   \item{lat}{latitude of station}
+#'   \item{lon}{longitude of station}
+#'   \item{station}{station name}
+#'   \item{effSkate}{effective skate number of the set, as calculated by IPHC}
+#'   \item{usable}{whether or not the set is usable, as determined by IPHC plus
+#'                 four extra that had no hooks enumerated except for Pacific
+#'                 Halibut, and so are not usable by us}
+#'
+#' }
+#' @source Originally from file 1995IPHCSetlineData.xlsx that Lynne Yamanaka
+#'         received from Aaron Ranta at the IPHC.
+"setData1995rs"
+
+#' Count data for 1995 IPHC survey for RS stations
+#'
+#' A dataset containing counts of each species for each RS (Random Sample?)
+#' station for the 1995 IPHC survey. We are not using these data
+#' but including here for completeness.
+#'
+#' @format A tibble:
+#' \describe{
+#'   \item{year}{1995 for this data set}
+#'   \item{station}{station name, to link with setData1995rs}
+#'   \item{specName}{species name for the counts for that set}
+#'   \item{specCount}{count of that species for that set}
+#' }
+#' @source Originally from file 1995IPHCSetlineData.xlsx that Lynne Yamanaka
+#'         received from Aaron Ranta at the IPHC.
+"countData1995rs"
