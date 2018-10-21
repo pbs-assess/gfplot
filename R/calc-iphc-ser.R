57cc2723c59c31d3a81e413f315d1ae9d4aa596c
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-10-21 12:42:04 -0700

    Simpler fix, just look at intersecting years.

--- R/calc-iphc-ser.R
+++ R/calc-iphc-ser.R
@@ -313,7 +313,8 @@ calc_iphc_ser_AB <- function(series_all) {
 ##' @param series_all List of tibbles, one for each of Series A, B, C and D,
 ##'   resulting from [calc_iphc_ser_all()]
 ##' @return List of t_AD (results of the paired t-test), and geometric means G_A
-##'   and G_D. t_AD is NULL if years are the same in Series A and Series D.
+##'   and G_D. t_AD is NULL if there is no catch in the overlapping years for
+##'   Series A or Series D.
 ##'
 compare_iphc_ser_A_D <- function(series_all) {
     years_AD <- intersect(series_all$ser_A$year, series_all$ser_D$year)
@@ -328,16 +329,27 @@ compare_iphc_ser_A_D <- function(series_all) {
                                   I_t20BootMean > 0)$I_t20BootMean)))
     # If Series A and Series D all have 0 counts for al the intersecting years
     #  then return NULL for t_AD (else t-test fails), e.g. Darkblotched.
-    if(length(unique(series_all$ser_A$year)) ==
-                                  length( unique(series_all$ser_D$year) ) ){
-       if(all(unique(series_all$ser_A$year) == unique(series_all$ser_D$year))) {
-           if( max( c( series_all$ser_A$I_t20SampleMean,
-                       series_all$ser_D$I_t20SampleMean) ) == 0) {
-              return(list(t_AD = NULL,
-                          G_A = G_A,
-                          G_D = G_D) )
-           }
-        }
+    #if(length(unique(series_all$ser_A$year)) ==
+    #                              length( unique(series_all$ser_D$year) ) ){
+    #   if(all(unique(series_all$ser_A$year) == unique(series_all$ser_D$year))) {
+    #       if( max( c( series_all$ser_A$I_t20SampleMean,
+    #                   series_all$ser_D$I_t20SampleMean) ) == 0) {
+    #          return(list(t_AD = NULL,
+    #                      G_A = G_A,
+    #                      G_D = G_D) )
+    #       }
+    #    }
+    #
+
+    #Actually just want the intersecting years:
+
+    if( max( c( filter(series_all$ser_A,
+                       year %in% years_AD)$I_t20SampleMean,
+                filter(series_all$ser_D,
+                       year %in% years_AD)$I_t20SampleMean) ) == 0 ) {
+       return(list(t_AD = NULL,
+                   G_A = G_A,
+                   G_D = G_D) )
     }
 
     # Scale by G_A, geometricetric mean of bootstrapped means.
