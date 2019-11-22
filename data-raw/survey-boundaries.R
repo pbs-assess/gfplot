148025ea3c4d9cdbf2e7eb398ee6187ddc4adf99
Sean Anderson <sean@seananderson.ca>
2019-11-21 17:30:18 -0800

    Add dogfish grid

--- data-raw/survey-boundaries.R
+++ data-raw/survey-boundaries.R
@@ -128,3 +128,48 @@ hbll_inside_s_grid <- list(grid = hbll_inside_s_grid, cell_area = 2.0)
 usethis::use_data(hbll_inside_s_grid, internal = FALSE, overwrite = TRUE)
 
 setwd("../../../")
+
+# dogfish ----------------------------
+
+setwd("inst/extdata/dogfish")
+
+library(sf)
+library(ggplot2)
+
+# read nc polygon data and transform to UTM
+nc <- st_read(".") %>%
+  st_transform(26909)
+
+# # random sample of 5 points
+# pts <- st_sample(nc, size = 5) %>% st_sf
+
+# create 1km grid
+grid_2 <- st_make_grid(nc, cellsize = c(1000, 1000)) %>%
+  st_sf(grid_id = 1:length(.))
+
+# create labels for each grid_id
+grid_lab <- st_centroid(grid_2) %>% cbind(st_coordinates(.))
+
+# view the polygons and grid
+# ggplot() +
+#   geom_sf(data = nc, fill = 'white', lwd = 0.05) +
+#   # geom_sf(data = pts, color = 'red', size = 1.7) +
+#   geom_sf(data = grid_2, fill = 'transparent', lwd = 0.3) +
+#   # geom_text(data = grid_lab, aes(x = X, y = Y, label = grid_id), size = 2) +
+#   coord_sf(datum = NA)  +
+#   labs(x = "") +
+#   labs(y = "")
+
+# which grid square is each point in?
+.inside <- nc %>% st_join(grid_2, join = st_intersects) %>% as.data.frame
+
+dogfish_grid <- grid_2[.inside$grid_id,] %>%
+  st_coordinates() %>%
+  as.data.frame() %>%
+  select(X, Y) %>%
+  mutate(X = X/1000, Y = Y/1000)
+
+dogfish_grid <- list(grid = dogfish_grid, cell_area = 1)
+usethis::use_data(dogfish_grid, internal = FALSE, overwrite = TRUE)
+
+setwd("../../../")
