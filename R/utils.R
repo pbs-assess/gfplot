ebeba7099948301a6008c2397eb9ae7cb0db7e15
Sean Anderson <sean@seananderson.ca>
2022-06-16 17:26:49 -0700

    Remove 2 more imports

--- R/utils.R
+++ R/utils.R
@@ -1,44 +1,3 @@
-#' Run SQL
-#'
-#' @param database The name of the database.
-#' @param query The query to run.
-#'
-#' @details
-#' If you need to use a user-password setup to access the databases, then you
-#' will need to set the R options in your .Rprofile file: `pbs.uid`, `pbs.pwd`,
-#' `pbs.ip`, `pbs.sqldriver`. E.g. `options(pbs.uid="MyUserName")` The default
-#' SQL driver will be `"SQL Server"` if not specified in the options. This will
-#' probably work for most people. You might try using
-#' `usethis::edit_r_profile()` if you need help finding your R profile file.
-#'
-#' @export
-run_sql <- function(database, query) {
-  query <- paste(query, collapse = "\n")
-  con <- db_connection(database = database)
-  on.exit(DBI::dbDisconnect(con))
-  DBI::dbGetQuery(con, query)
-}
-
-db_connection <- function(server = "DFBCV9TWVASP001",
-                          database = "GFBioSQL") {
-  pbs_uid <- getOption("pbs.uid")
-  pbs_pwd <- getOption("pbs.pwd")
-  pbs_ip <- getOption("pbs.ip")
-  pbs_sqldriver <- getOption("pbs.sqldriver")
-  if (!is.null(pbs_uid) && !is.null(pbs_pwd) && !is.null(pbs_ip)) {
-    DBI::dbConnect(odbc::odbc(),
-      driver = if (is.null(pbs_sqldriver)) "SQL Server" else pbs_sqldriver,
-      server = pbs_ip, database = database,
-      pwd = pbs_pwd, uid = pbs_uid
-    )
-  } else {
-    DBI::dbConnect(odbc::odbc(),
-      driver = "SQL Server",
-      server = server, database = database
-    )
-  }
-}
-
 force_three_letter_species_code <- function(x) {
   if (is.numeric(x)) {
     sprintf(paste0("%0", 3L, "d"), x)
@@ -49,77 +8,6 @@ force_three_letter_species_code <- function(x) {
 
 all_species_codes <- function(x) {
   all(grepl("[0-9]+", x))
-}
-
-common2codes <- function(common) {
-  if (all_species_codes(common)) {
-    return(force_three_letter_species_code(common))
-  }
-
-  species <- DBI::dbGetQuery(
-    db_connection(database = "GFBioSQL"),
-    "SELECT * FROM SPECIES"
-  )
-  common_df <- data.frame(
-    SPECIES_COMMON_NAME = toupper(common),
-    order_by = seq_along(common), stringsAsFactors = FALSE
-  )
-  .d <- filter(species, SPECIES_COMMON_NAME %in% toupper(common))
-  # Remove erroneous species codes for basking shark and lingcod:
-  .d <- filter(.d, !SPECIES_CODE %in% c("033", "465")) %>%
-    left_join(common_df, by = "SPECIES_COMMON_NAME") %>%
-    arrange(.data$order_by)
-  .d$SPECIES_CODE
-}
-
-codes2common <- function(spp_code) {
-  species <- DBI::dbGetQuery(
-    db_connection(database = "GFBioSQL"),
-    "SELECT * FROM SPECIES"
-  )
-  code_df <- data.frame(
-    SPECIES_CODE = spp_code,
-    order_by = seq_along(spp_code), stringsAsFactors = FALSE
-  )
-  common_df <- data.frame(
-    SPECIES_CODE = as.character(spp_code),
-    order_by = seq_along(spp_code), stringsAsFactors = FALSE
-  )
-  .d <- filter(species, SPECIES_CODE %in% spp_code)
-  # Remove erroneous species codes for basking shark and lingcod:
-  .d <- filter(.d, !SPECIES_CODE %in% c("033", "465")) %>%
-    left_join(common_df, by = "SPECIES_CODE") %>%
-    arrange(.data$order_by)
-  .d$SPECIES_COMMON_NAME
-}
-
-collapse_filters <- function(x) {
-  paste0("'", paste(x, collapse = "','"), "'")
-}
-
-#' gfplot utilities
-#'
-#' @param sql_precode SQL go to inject before a species list etc.
-#' @param species A vector of species or similar objects.
-#' @param sql_code The SQL go to operate on.
-#' @param search_flag What to search for in terms of where to inject the code.
-#' @param conversion_func A conversion function to apply to the species or
-#'   related vector.
-#' @export
-inject_filter <- function(sql_precode, species, sql_code,
-                          search_flag = "-- insert species here",
-                          conversion_func = common2codes) {
-  i <- grep(search_flag, sql_code)
-  sql_code[i] <- paste0(
-    sql_precode, " (",
-    collapse_filters(conversion_func(species)), ")"
-  )
-  sql_code
-}
-
-firstup <- function(x) {
-  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
-  x
 }
 
 first_cap <- function(s, strict = FALSE) {
@@ -134,14 +22,6 @@ first_cap <- function(s, strict = FALSE) {
     )
   }
   sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
-}
-
-read_sql <- function(x) {
-  if (file.exists(system.file("sql", x, package = "gfplot"))) {
-    readLines(system.file("sql", x, package = "gfplot"))
-  } else {
-    stop("The sql file does not exist.")
-  }
 }
 
 round_any <- function(x, accuracy) {
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,47 +1,47 @@
 context("Utility functions work as expected.")
 
-test_that("all_species_codes() works", {
-  expect_false(all_species_codes("a"))
-  expect_false(all_species_codes(c("a", "b")))
-  expect_true(all_species_codes(1))
-  expect_true(all_species_codes(c(1, 1)))
-  expect_true(all_species_codes(c("1", "2")))
-  expect_false(all_species_codes(c("a", 1)))
-  expect_false(all_species_codes(c("a", "1")))
-  expect_false(all_species_codes(c("a", "a1")))
-})
+# test_that("all_species_codes() works", {
+#   expect_false(all_species_codes("a"))
+#   expect_false(all_species_codes(c("a", "b")))
+#   expect_true(all_species_codes(1))
+#   expect_true(all_species_codes(c(1, 1)))
+#   expect_true(all_species_codes(c("1", "2")))
+#   expect_false(all_species_codes(c("a", 1)))
+#   expect_false(all_species_codes(c("a", "1")))
+#   expect_false(all_species_codes(c("a", "a1")))
+# })
 
-test_that("force_three_letter_species_code() works", {
-  expect_equal(force_three_letter_species_code(3), "003")
-  expect_equal(force_three_letter_species_code(c(1, 2)), c("001", "002"))
-  expect_equal(force_three_letter_species_code(03), "003")
-  expect_equal(force_three_letter_species_code("4004"), "4004")
-  expect_equal(force_three_letter_species_code("003"), "003")
-  expect_equal(force_three_letter_species_code("abc"), "abc")
-})
+# test_that("force_three_letter_species_code() works", {
+#   expect_equal(force_three_letter_species_code(3), "003")
+#   expect_equal(force_three_letter_species_code(c(1, 2)), c("001", "002"))
+#   expect_equal(force_three_letter_species_code(03), "003")
+#   expect_equal(force_three_letter_species_code("4004"), "4004")
+#   expect_equal(force_three_letter_species_code("003"), "003")
+#   expect_equal(force_three_letter_species_code("abc"), "abc")
+# })
 
-test_that("common2codes() works", {
-  expect_equal(common2codes(3), "003")
-  expect_equal(common2codes(c(1, 2)), c("001", "002"))
-  expect_equal(common2codes(c("001", "002")), c("001", "002"))
-})
+# test_that("common2codes() works", {
+#   expect_equal(common2codes(3), "003")
+#   expect_equal(common2codes(c(1, 2)), c("001", "002"))
+#   expect_equal(common2codes(c("001", "002")), c("001", "002"))
+# })
 
-test_that("collapse_filters() works", {
-  expect_equal(collapse_filters(c(1, 2)), "'1','2'")
-})
+# test_that("collapse_filters() works", {
+#   expect_equal(collapse_filters(c(1, 2)), "'1','2'")
+# })
 
-test_that("inject_filter() works", {
-  x <- inject_filter("a",
-    species = "b",
-    sql_code = list("y", "\n-- insert here", "z"),
-    search_flag = "-- insert here",
-    conversion_func = I
-  )
-
-  expect_equal(x[[1]], "y")
-  expect_equal(x[[2]], "a ('b')")
-  expect_equal(x[[3]], "z")
-})
+# test_that("inject_filter() works", {
+#   x <- inject_filter("a",
+#     species = "b",
+#     sql_code = list("y", "\n-- insert here", "z"),
+#     search_flag = "-- insert here",
+#     conversion_func = I
+#   )
+#
+#   expect_equal(x[[1]], "y")
+#   expect_equal(x[[2]], "a ('b')")
+#   expect_equal(x[[3]], "z")
+# })
 
 test_that("firstup() works", {
   expect_equal(firstup("abc"), "Abc")
