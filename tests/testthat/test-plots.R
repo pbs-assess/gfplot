3f23bb236453f7e74307d1c5338fc538c32c7044
Jillian Dunic <jdunic@gmail.com>
2026-01-06 12:49:39 -0800

    fix broken test

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -4,5 +4,7 @@ g <- plot_age_precision(dplyr::tibble(
   prec_age = 2.0, prim_age = 1.9,
   prec_min_age = 1.5, prec_max_age = 2.5,
   prim_min_age = 1.5, prim_max_age = 2.9))
-expect_identical(class(g), c("gg", "ggplot"))
+
+expect_true(inherits(g, "gg"))
+expect_true(inherits(g, "ggplot"))
 
